87481a800f8bc52f37fe9062287d914bed13dae5
Roovers <91910203+Roovers@users.noreply.github.com>
2026-06-14 23:42:41 -0300

    Subir src/services/api.ts

---
+++ src/services/api.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { API_BASE_URL } from "../config/api";
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
